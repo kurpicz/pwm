aee2330983a234f58465c9a3ebc322ec50d1f89e
Marvin Löbel <loebel.marvin@gmail.com>
2017-07-04 12:56:19 +0200

    Add initial tests

--- test/test_template.cpp
+++ test/test_template.cpp
@@ -1,12 +1,51 @@
 #include <gtest/gtest.h>
 #include "test/util.hpp"
 
+#include <unordered_map>
+
 // WX_NAME is defined by the cmake file to be one of the algorithms
 #include "@WX_NAME@.hpp"
 #define WX_NAME @WX_NAME@
 
-void foo(WX_NAME<size_t, size_t> bar) {}
+template <typename AlphabetType>
+auto ConstructWM(std::vector<AlphabetType>& text, const bool already_reduced) {
+    uint64_t max_char = 0;
+    if (!already_reduced) {
+        std::unordered_map<AlphabetType, uint64_t> word_list;
+        for (const AlphabetType& character : text) {
+        auto result = word_list.find(character);
+        if (result == word_list.end()) {
+            word_list.emplace(character, max_char++);
+        }
+        }
+        --max_char;
+        for (uint64_t i = 0; i < text.size(); ++i) {
+        text[i] = static_cast<AlphabetType>(word_list.find(text[i])->second);
+        }
+    } else {
+        for (const AlphabetType& character : text) {
+        if (character > max_char) {
+            max_char = character;
+        }
+        }
+    }
+    std::cout << "Greatest character: " << static_cast<uint64_t>(max_char)
+                << std::endl;
+
+    AlphabetType levels = 0;
+    while (max_char) {
+        max_char >>= 1;
+        ++levels;
+    }
+    std::cout << "Levels in WM: " << static_cast<uint64_t>(levels) << std::endl;
+
+    return WX_NAME<AlphabetType, uint32_t>(text, text.size(), levels);
+}
 
 TEST(WX_NAME, smoketest) {
-    //std::cout << "foo\n";
+    test::roundtrip_batch([](const std::string& s){
+        auto vec = std::vector<uint8_t>(s.begin(), s.end());
+        ConstructWM(vec, false);
+        ConstructWM(vec, true);
+    });
 }
