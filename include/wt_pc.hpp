195975fde324f9e651397eb72cc4ded9da97c9aa
Marvin Löbel <loebel.marvin@gmail.com>
2017-07-21 14:59:01 +0200

    add headers

--- include/wt_pc.hpp
+++ include/wt_pc.hpp
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * include/wt_pc.hpp
+ *
+ * Copyright (C) 2017 Florian Kurpicz <florian.kurpicz@tu-dortmund.de>
+ *
+ * All rights reserved. Published under the BSD-2 license in the LICENSE file.
+ ******************************************************************************/
+
 #pragma once
 
 #include "wx_pc.hpp"
