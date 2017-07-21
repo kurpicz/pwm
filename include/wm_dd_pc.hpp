57573c4a6d32f3854c143fd300ca17e51a7535eb
Marvin Löbel <loebel.marvin@gmail.com>
2017-07-21 14:19:31 +0200

    Replace wm_dd_pc impl with common pc one

--- include/wm_dd_pc.hpp
+++ include/wm_dd_pc.hpp
@@ -53,8 +53,6 @@ public:
             const SizeType omp_size = omp_get_num_threads();
             assert(omp_size == shards);
 
-            auto& ctx = ctxs[omp_rank];
-
             const SizeType local_size = (size / omp_size) +
                 ((omp_rank < size % omp_size) ? 1 : 0);
             const SizeType offset = (omp_rank * (size / omp_size)) +
@@ -62,85 +60,7 @@ public:
 
             const AlphabetType* text = global_text.data() + offset;
 
-            SizeType cur_max_char = (1 << levels);
-
-            auto& zeros = ctx.zeros();
-            auto& borders = ctx.borders();
-            auto& bv = ctx.bv().vec();
-
-            // While initializing the histogram, we also compute the fist level
-            SizeType cur_pos = 0;
-            for (; cur_pos + 64 <= local_size; cur_pos += 64) {
-                uint64_t word = 0ULL;
-                for (SizeType i = 0; i < 64; ++i) {
-                    ++ctx.hist(levels, text[cur_pos + i]);
-                    word <<= 1;
-                    word |= ((text[cur_pos + i] >> (levels - 1)) & 1ULL);
-                }
-                bv[0][cur_pos >> 6] = word;
-            }
-            if (local_size & 63ULL) {
-                uint64_t word = 0ULL;
-                for (SizeType i = 0; i < local_size - cur_pos; ++i) {
-                    ++ctx.hist(levels, text[cur_pos + i]);
-                    word <<= 1;
-                    word |= ((text[cur_pos + i] >> (levels - 1)) & 1ULL);
-                }
-                word <<= (64 - (local_size & 63ULL));
-                bv[0][local_size >> 6] = word;
-            }
-
-            // The number of 0s at the last level is the number of "even" characters
-            if (ctx.calc_zeros()) {
-                for (SizeType i = 0; i < cur_max_char; i += 2) {
-                    zeros[levels - 1] += ctx.hist(levels, i);
-                }
-            }
-
-            // Now we compute the WM bottom-up, i.e., the last level first
-            for (SizeType level = levels - 1; level > 0; --level) {
-                const SizeType prefix_shift = (levels - level);
-                const SizeType cur_bit_shift = prefix_shift - 1;
-
-                // Update the maximum value of a feasible a bit prefix and update the
-                // histogram of the bit prefixes
-                cur_max_char >>= 1;
-
-                for (SizeType i = 0; i < cur_max_char; ++i) {
-                    ctx.hist(level, i)
-                        = ctx.hist(level + 1, i << 1)
-                        + ctx.hist(level + 1, (i << 1) + 1);
-                }
-
-                // Compute the starting positions of characters with respect to their
-                // bit prefixes and the bit-reversal permutation
-                borders[0] = 0;
-                for (SizeType i = 1; i < cur_max_char; ++i) {
-                    auto const prev_rho = ctx.rho(level, i - 1);
-
-                    borders[ctx.rho(level, i)] = borders[prev_rho] + ctx.hist(level, prev_rho);
-
-                    if (ctx.calc_rho())  {
-                        ctx.set_rho(level - 1, i - 1, prev_rho >> 1);
-                    }
-                }
-
-                // The number of 0s is the position of the first 1 in the previous level
-                if (ctx.calc_zeros()) {
-                    zeros[level - 1] = borders[1];
-                }
-
-                // Now we insert the bits with respect to their bit prefixes
-                for (SizeType i = 0; i < local_size; ++i) {
-                    const SizeType pos = borders[text[i] >> prefix_shift]++;
-                    bv[level][pos >> 6] |= (((text[i] >> cur_bit_shift) & 1ULL)
-                        << (63ULL - (pos & 63ULL)));
-                }
-            }
-
-            if (levels > 1) { // TODO check condition
-                ctx.hist(0, 0) = ctx.hist(1, 0) + ctx.hist(1, 1);
-            }
+            pc(text, local_size, levels, ctxs[omp_rank]);
         }
 
         auto glob_bv = std::vector<Bvs<SizeType>>(
