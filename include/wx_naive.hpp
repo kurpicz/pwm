d3575d599935ebeded4e437f855af76cf7b061a6
Florian Kurpicz <florian.kurpicz@tu-dortmund.de>
2017-07-22 17:25:54 +0200

    Unifying naive WM and WT construction

--- include/wx_naive.hpp
+++ include/wx_naive.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * include/wm_naive.hpp
+ * include/wx_naive.hpp
  *
  * Copyright (C) 2017 Florian Kurpicz <florian.kurpicz@tu-dortmund.de>
  *
@@ -7,20 +7,72 @@
  ******************************************************************************/
 
 #pragma once
-#ifndef WM_NAIVE
-#define WM_NAIVE
 
 #include <cstring>
-#include <vector>
 
-#include "common.hpp"
+template <typename AlphabetType, bool is_matrix>
+class wx_naive;
 
-template <typename AlphabetType, typename SizeType>
-class wm_naive {
+template <typename AlphabetType>
+class wx_naive<AlphabetType, false> {
 
 public:
-  wm_naive(const std::vector<AlphabetType>& text, const SizeType size,
-    const SizeType levels) : _bv(levels), _zeros(levels, 0) {
+  wx_naive(const std::vector<AlphabetType>& text, const uint64_t size,
+    const uint64_t levels) : _bv(levels) {
+
+    std::vector<AlphabetType> local_text = text;
+
+    for (uint64_t level = 0; level < levels; ++level) {
+      _bv[level] = new uint64_t[(size + 63ULL) >> 6];
+      memset(_bv[level], 0, ((size + 63ULL) >> 6) * sizeof(uint64_t));
+
+      uint32_t cur_pos = 0;
+      for (; cur_pos + 64 <= size; cur_pos += 64) {
+        uint64_t word = 0ULL;
+        for (uint32_t i = 0; i < 64; ++i) {
+          word <<= 1;
+          word |= ((local_text[cur_pos + i] >> (levels - (level + 1))) & 1ULL);
+        }
+        _bv[level][cur_pos >> 6] = word;
+      }
+      if (size & 63ULL) {
+        uint64_t word = 0ULL;
+        for (uint32_t i = 0; i < size - cur_pos; ++i) {
+          word <<= 1;
+          word |= ((local_text[cur_pos + i] >> (levels - (level + 1))) & 1ULL);
+        }
+        word <<= (64 - (size & 63ULL));
+        _bv[level][size >> 6] = word;
+      }
+
+      std::vector<std::vector<AlphabetType>> buckets(1ULL << (level + 1));
+      for (uint64_t i = 0; i < local_text.size(); ++i) {
+        buckets[local_text[i] >> (levels - (level + 1))]
+          .emplace_back(local_text[i]);
+      }
+      cur_pos = 0;
+      for (const auto& bucket : buckets) {
+        for (const auto character : bucket) {
+          local_text[cur_pos++] = character;
+        }
+      }
+    }
+  }
+
+  auto get_bv() const {
+    return _bv;
+  }
+
+private:
+  std::vector<uint64_t*> _bv;
+}; // class wt_naive
+
+template <typename AlphabetType>
+class wx_naive<AlphabetType, true> {
+
+public:
+  wx_naive(const std::vector<AlphabetType>& text, const uint64_t size,
+    const uint64_t levels) : _bv(levels), _zeros(levels, 0) {
 
     if(text.size() == 0) { return; }
 
@@ -80,9 +132,7 @@ public:
 
 private:
   std::vector<uint64_t*> _bv;
-  std::vector<SizeType> _zeros;
-}; // class wm_naive
-
-#endif // WM_NAIVE
+  std::vector<uint64_t> _zeros;
+}; // class wx_naive<MATRIX>
 
 /******************************************************************************/
---
+++ src/naive.cpp
@@ -0,0 +1,12 @@
+/*******************************************************************************
+ * src/naive.cpp
+ *
+ * Copyright (C) 2017 Florian Kurpicz <florian.kurpicz@tu-dortmund.de>
+ *
+ * All rights reserved. Published under the BSD-2 license in the LICENSE file.
+ ******************************************************************************/
+
+#include "benchmark/algorithm.hpp"
+#include "wx_naive.hpp"
+
+/******************************************************************************/
