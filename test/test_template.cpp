670a2a79e9acecc83882b944968c4bc9c7822253
Marvin Löbel <loebel.marvin@gmail.com>
2017-07-24 15:40:04 +0200

    Show which algorithms are tested

--- test/test_template.cpp
+++ test/test_template.cpp
@@ -19,6 +19,7 @@ TEST(wavelet_construction, smoketest) {
   auto& algo_list = algorithm_list::get_algorithm_list();
   for (const auto& a : algo_list) {
     if (a->word_width() == 1) {
+      a->print_info();
       test::roundtrip_batch([&](const std::string& s){
         auto vec = std::vector<uint8_t>(s.begin(), s.end());
         uint64_t levels = no_reduction_alphabet(vec);
