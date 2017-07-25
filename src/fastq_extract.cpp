1cf2f470df567383758789991d6b53b1f41da96a
Marvin Löbel <loebel.marvin@gmail.com>
2017-07-25 15:34:22 +0200

    add small fastq extract tool

---
+++ src/fastq_extract.cpp
@@ -0,0 +1,68 @@
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+#include <cstdint>
+#include <array>
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        std::cerr << "usage: prog FILE\n";
+        return 1;
+    }
+
+    std::ifstream myfile (argv[1]);
+
+        char c;
+    size_t pos = 0;
+
+    auto check = [&](auto c_should) {
+        if (c != c_should) {
+            std::cerr << "File err @" << pos << ": " << c << "should be " << c_should << "\n";
+            std::exit(1);
+        }
+    };
+
+    if (myfile.is_open())
+    {
+
+        while(myfile) {
+            check('@');
+
+            while(myfile.get(c)) {
+                pos++;
+                if (c == '\n') {
+                    break;
+                }
+            }
+
+            while(myfile.get(c)) {
+                pos++;
+                if (c == '\n') {
+                    break;
+                }
+                std::cout << c;
+            }
+
+            check('+');
+
+            while(myfile.get(c)) {
+                pos++;
+                if (c == '\n') {
+                    break;
+                }
+            }
+
+            while(myfile.get(c)) {
+                pos++;
+                if (c == '\n') {
+                    break;
+                }
+            }
+        }
+
+        myfile.close();
+    }
+
+
+    return 0;
+}
