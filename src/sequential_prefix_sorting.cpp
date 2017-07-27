47e05d3cbc20ae0ef65bfc0a61e2923efaa05404
Marvin Löbel <loebel.marvin@gmail.com>
2017-07-27 16:53:11 +0200

    fix typo

--- src/sequential_prefix_sorting.cpp
+++ src/sequential_prefix_sorting.cpp
@@ -14,13 +14,13 @@ using wm_ps_16 = wx_ps<uint16_t, true>;
 using wm_ps_32 = wx_ps<uint32_t, true>;
 
 CONSTRUCTION_REGISTER("wm_ps<uint8_t>",
-  "Sqeuential wavelet matrix construction with 8-bit alphabet (using sorting).",
+  "Sequential wavelet matrix construction with 8-bit alphabet (using sorting).",
   wm_ps_8)
 CONSTRUCTION_REGISTER("wm_ps<uint16_t>",
-  "Sqeuential wavelet matrix construction with 16-bit alphabet (using sorting).",
+  "Sequential wavelet matrix construction with 16-bit alphabet (using sorting).",
   wm_ps_16)
 CONSTRUCTION_REGISTER("wm_ps<uint32_t>",
-  "Sqeuential wavelet matrix construction with 32-bit alphabet (using sorting).",
+  "Sequential wavelet matrix construction with 32-bit alphabet (using sorting).",
   wm_ps_32)
 
 using wt_ps_8 = wx_ps<uint8_t, false>;
@@ -28,13 +28,13 @@ using wt_ps_16 = wx_ps<uint16_t, false>;
 using wt_ps_32 = wx_ps<uint32_t, false>;
 
 CONSTRUCTION_REGISTER("wt_ps<uint8_t>",
-  "Sqeuential wavelet tree construction with 8-bit alphabet (using sorting).",
+  "Sequential wavelet tree construction with 8-bit alphabet (using sorting).",
   wt_ps_8)
 CONSTRUCTION_REGISTER("wt_ps<uint16_t>",
-  "Sqeuential wavelet tree construction with 16-bit alphabet (using sorting).",
+  "Sequential wavelet tree construction with 16-bit alphabet (using sorting).",
   wt_ps_16)
 CONSTRUCTION_REGISTER("wt_ps<uint32_t>",
-  "Sqeuential wavelet tree construction with 32-bit alphabet (using sorting).",
+  "Sequential wavelet tree construction with 32-bit alphabet (using sorting).",
   wt_ps_32)
 
 /******************************************************************************/
