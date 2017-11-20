4456cfcce845f4b823138a5606e6edf9482b29d4
Florian Kurpicz <florian.kurpicz@tu-dortmund.de>
2017-11-20 14:17:32 +0100

    Add level_size function

---
+++ include/huffman/README.md
@@ -0,0 +1,11 @@
+# Huffman-shaped Wavelet Trees and Wavelet Matrices
+
+Here, we extended our algorithms such that their output is a Huffman-shaped wavelet tree (HWT) or Huffman-shaped wavelet matrix (HWM).
+To this end, we compute the Huffman codes for all symbols in our input and compute the bit vectors for the codes.
+Hence, the size of the levels of the tree/matrix will be decreasing.
+The first level of a HWT/HWM contains the exactly as many bits as the first level of a WT/WM.
+
+We have implemented:
+1. TODO
+
+Replace _wx_ with either _hwt_ or _hwm_ for the corresponding HWT- or HWM-construction algorithm.
--- include/util/flat_two_dim_array.hpp
+++ include/util/flat_two_dim_array.hpp
@@ -20,15 +20,14 @@ public:
 
   template <typename... SizeFunctionArgs>
   flat_two_dim_array(const uint64_t levels, SizeFunctionArgs... size_f_args)
-  : data_(levels) {
-    assert(levels > 0);
+  : data_(levels + 1) {
     uint64_t data_size = 0;
     for (uint64_t level = 0; level < levels; ++level) {
       data_size += size_function::level_size(level, size_f_args...);
     }
     data_[0] = new IndexType[data_size];
     memset(data_[0], 0, data_size * sizeof(IndexType));
-    for (uint64_t level = 1; level < levels; ++level) {
+    for (uint64_t level = 1; level < levels + 1; ++level) {
       data_[level] = data_[level - 1] +
         size_function::level_size(level - 1, size_f_args...);
     }
@@ -39,7 +38,7 @@ public:
 
   flat_two_dim_array& operator =(flat_two_dim_array&& other) {
     if (*this != other) {
-      if (data_.size() > 0) {
+      if (levels() > 0) {
         delete[] data_[0];
       }
       data_ = std::move(other.data_);
@@ -57,12 +56,12 @@ public:
   flat_two_dim_array& operator =(const flat_two_dim_array&) = delete;
 
   bool operator ==(const flat_two_dim_array& other) const {
-    if ((data_.size() == 0 && other.data_.size() != 0) ||
-        (data_.size() != 0 && other.data_.size() == 0)) {
+    if ((levels() == 0 && other.levels() != 0) ||
+        (levels() != 0 && other.levels() == 0)) {
       return false;
     }
     // Here we know that either both have length 0 or both have length > 0.
-    return ((data_.size() == 0 && other.data_.size() == 0) ||
+    return ((levels() == 0 && other.levels() == 0) ||
       data_[0] == other.data_[0]);
   }
 
@@ -71,7 +70,11 @@ public:
   }
 
   inline uint64_t levels() const {
-    return data_.size();
+    return data_.size() - 1;
+  }
+
+  inline uint64_t level_size(uint64_t level) const {
+    return data_[level + 1] - data_[level];
   }
 
   inline const IndexType* operator [](const uint64_t index) const {
