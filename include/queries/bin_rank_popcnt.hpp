fc2eaba15d089011f489748e1c0ecaeab55f2a19
Florian Kurpicz <florian.kurpicz@tu-dortmund.de>
2018-09-03 12:18:54 +0200

    Rename blocks for more descriptive names

--- include/queries/bin_rank_popcnt.hpp
+++ include/queries/bin_rank_popcnt.hpp
@@ -32,8 +32,8 @@ class bin_rank_popcnt {
 
 public:
   bin_rank_popcnt(uint64_t const * data, const size_t size)
-  : l0_((size / upper_block_cover_) + 1, 0ULL),
-    l12_((size / l12_block_cover_) + 1, 0ULL),
+  : l0_((size / l0_block_cover_) + 1, 0ULL),
+    l12_((size / l1_block_cover_) + 1, 0ULL),
     data_(data), size_(size) {
 
     auto cur_pos = data;
@@ -47,17 +47,17 @@ public:
       l12_[l12_pos] = set_l1_entry(l12_[l12_pos], l1_entry);
       for (size_t i = 0; i < 3; ++i) {
         uint32_t l2_entry = 0UL;
-        for (size_t j = 0; j < basic_block_cover_; ++j) {
+        for (size_t j = 0; j < l2_block_cover_; ++j) {
           l2_entry += __builtin_popcountll(*(cur_pos++));
         }
         l1_entry += l2_entry;
         l12_[l12_pos] = set_l2_entry(l12_[l12_pos], l2_entry, i);        
       }
-      for (size_t j = 0; j < basic_block_cover_; ++j) {
+      for (size_t j = 0; j < l2_block_cover_; ++j) {
         l1_entry += __builtin_popcountll(*(cur_pos++));
       }
       ++l12_pos;
-      if (l12_pos % upper_block_cover_ == 0) {
+      if (l12_pos % l0_block_cover_ == 0) {
         if (l0_pos > 0) { l0_[l0_pos] += l0_[l0_pos - 1]; }
         l0_[l0_pos++] += l1_entry;
         l1_entry = 0UL;
@@ -76,24 +76,24 @@ public:
     size_t result = 0;
     size_t remaining_bits = index;
     // Find L0 block
-    size_t l0_pos = remaining_bits / upper_block_bit_size_;
-    remaining_bits -= (l0_pos * upper_block_bit_size_);
+    size_t l0_pos = remaining_bits / l0_bit_size_;
+    remaining_bits -= (l0_pos * l0_bit_size_);
     if (l0_pos > 0) { result += l0_[l0_pos - 1]; }
 
     // Find L1/L2 block
-    size_t l1_pos = remaining_bits / l12_block_bit_size_;
-    remaining_bits -= (l1_pos * l12_block_bit_size_);
+    size_t l1_pos = remaining_bits / l1_bit_size_;
+    remaining_bits -= (l1_pos * l1_bit_size_);
     const uint64_t l12_block = l12_[l1_pos];
     result += get_l1_entry(l12_block);
 
-    size_t l2_pos = remaining_bits / basic_block_bit_size_;
-    remaining_bits -= (l2_pos * basic_block_bit_size_);
+    size_t l2_pos = remaining_bits / l2_bit_size_;
+    remaining_bits -= (l2_pos * l2_bit_size_);
     for (size_t i = 0; i < l2_pos; ++i) {
       result += get_l2_entry(l12_block, i);
     }
 
-    uint64_t const * remaining_data = data_ + (l1_pos * l12_block_cover_) +
-      (l2_pos * basic_block_cover_);
+    uint64_t const * remaining_data = data_ + (l1_pos * l1_block_cover_) +
+      (l2_pos * l2_block_cover_);
     while (remaining_bits >= 64) {
       result += __builtin_popcountll(*(remaining_data++));
       remaining_bits -= 64;
@@ -137,17 +137,14 @@ private:
 
 private:
   static constexpr size_t bits_per_word_ = sizeof(uint64_t) * 8;
-  static constexpr size_t basic_block_bit_size_ = 512;
-  static constexpr size_t l12_block_bit_size_ = 4 * basic_block_bit_size_;
-  static constexpr size_t upper_block_bit_size_ =
+  static constexpr size_t l2_bit_size_ = 512;
+  static constexpr size_t l1_bit_size_ = 4 * l2_bit_size_;
+  static constexpr size_t l0_bit_size_ =
     std::numeric_limits<uint32_t>::max();
 
-  static constexpr size_t basic_block_cover_ =
-    basic_block_bit_size_ / bits_per_word_;
-  static constexpr size_t l12_block_cover_ =
-    l12_block_bit_size_ / bits_per_word_;
-  static constexpr size_t upper_block_cover_ =
-    upper_block_bit_size_ / bits_per_word_;
+  static constexpr size_t l2_block_cover_ = l2_bit_size_ / bits_per_word_;
+  static constexpr size_t l1_block_cover_ = l1_bit_size_ / bits_per_word_;
+  static constexpr size_t l0_block_cover_ = l0_bit_size_ / bits_per_word_;
 
   std::vector<uint64_t> l0_;
   std::vector<uint64_t> l12_;
