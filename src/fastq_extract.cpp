ac17f65214e5df3cd44e48d7648893bccb12b420
Florian Kurpicz <florian.kurpicz@tu-dortmund.de>
2018-09-06 09:43:15 +0200

    Apply code style

--- src/fastq_extract.cpp
+++ src/fastq_extract.cpp
@@ -5,72 +5,59 @@
 #include <array>
 
 int main(int argc, char **argv) {
-    if (argc < 2) {
-        std::cerr << "usage: prog FILE\n";
-        return 1;
+  if (argc < 2) {
+    std::cerr << "usage: prog FILE\n";
+    return 1;
+  }
+
+  std::ifstream myfile (argv[1]);
+
+  char c;
+  size_t pos = 0;
+
+  auto check = [&](auto c_should) {
+    if (c != c_should) {
+      std::cerr << "File err @" << pos << ": " << c << "should be "
+                << c_should << std::endl;
+      std::exit(1);
     }
+  };
 
-    std::ifstream myfile (argv[1]);
-
-        char c;
-    size_t pos = 0;
-
-    auto check = [&](auto c_should) {
-        if (c != c_should) {
-            std::cerr << "File err @" << pos << ": " << c << "should be " << c_should << "\n";
-            std::exit(1);
+  if (myfile.is_open()) {
+    while (myfile) {
+      bool f0 = true;
+      while (myfile.get(c)) {
+        if (f0) {
+          f0 = false;
+          check('@');
         }
-    };
+        pos++;
+        if (c == '\n') { break; }
+      }
 
-    if (myfile.is_open())
-    {
+      while (myfile.get(c)) {
+        pos++;
+        if (c == '\n') { break; }
+        if (c != 'N') { std::cout << c; }
+      }
 
-        while(myfile) {
-            bool f0 = true;
-            while(myfile.get(c)) {
-                if(f0) {
-                    f0 = false;
-                    check('@');
-                }
-                pos++;
-                if (c == '\n') {
-                    break;
-                }
-            }
-
-            while(myfile.get(c)) {
-                pos++;
-                if (c == '\n') {
-                    break;
-                }
-                if (c != 'N') {
-                    std::cout << c;
-                }
-            }
-
-            bool f1 = true;
-            while(myfile.get(c)) {
-                if(f1) {
-                    f1 = false;
-                    check('+');
-                }
-                pos++;
-                if (c == '\n') {
-                    break;
-                }
-            }
-
-            while(myfile.get(c)) {
-                pos++;
-                if (c == '\n') {
-                    break;
-                }
-            }
+      bool f1 = true;
+      while (myfile.get(c)) {
+        if (f1) {
+          f1 = false;
+          check('+');
         }
+        pos++;
+        if (c == '\n') { break; }
+      }
 
-        myfile.close();
+      while (myfile.get(c)) {
+        pos++;
+        if (c == '\n') { break; }
+      }
     }
+    myfile.close();
+  }
 
-
-    return 0;
+  return 0;
 }
