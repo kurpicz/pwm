1c04110b6b91767cec60f97df8ab6e933332e4e7
Marvin Löbel <loebel.marvin@gmail.com>
2018-09-10 13:20:47 +0200

    Add a debug print for normal lists of values

--- include/util/print.hpp
+++ include/util/print.hpp
@@ -26,11 +26,54 @@ static void print_structure(std::ostream& out, wavelet_structure const& structur
     }
     out << "]";
     if (!structure.is_tree() && i != (bv.levels() - 1)) {
-        out << " zeros[" << i << "] = " << zeros.at(i);
+      out << " zeros[" << i << "] = " << zeros.at(i);
     }
     out << std::endl;
   }
   out << std::endl;
+}
+
+template<typename T>
+struct force_integer_trait {
+  inline static std::ostream& print(std::ostream& out, T const& v) {
+      return out << v;
+  }
+};
+template<>
+struct force_integer_trait<uint8_t> {
+  inline static std::ostream& print(std::ostream& out, uint8_t const& v) {
+    return out << int(v);
+}
+};
+/// Print T, but convert it to int first if it is a char-like type
+template<typename T>
+inline std::ostream& print_force_integer(std::ostream& out, T const& t) {
+  return force_integer_trait<T>::print(out, t);
+}
+
+template<typename list_type>
+static std::ostream& print_list(std::ostream& out, list_type const& list, bool nice = false) {
+    if (!nice) {
+        out << "[";
+        for(size_t i = 0; i < list.size(); i++) {
+            if (i > 0) {
+                out << ", ";
+            }
+            print_force_integer(out, list[i]);
+        }
+        out << "]";
+    } else {
+        out << "[\n";
+        for(size_t i = 0; i < list.size(); i++) {
+            if (i > 0) {
+                out << ",\n";
+            }
+            out << "    ";
+            print_force_integer(out, list[i]);
+        }
+        out << "\n]\n";
+    }
+    return out;
 }
 
 /******************************************************************************/
