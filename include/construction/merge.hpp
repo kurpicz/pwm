b1c9f494e61b3e43c94b182de0bbbf0dd5a66a87
Marvin Löbel <loebel.marvin@gmail.com>
2018-09-19 15:21:45 +0200

    Refactor & annotate merge.hpp

--- include/construction/merge.hpp
+++ include/construction/merge.hpp
@@ -14,6 +14,7 @@
 
 #include "arrays/bit_vectors.hpp"
 #include "util/common.hpp"
+#include "util/macros.hpp"
 
 template<typename WordType>
 void copy_bits(WordType* const dst, WordType const* const src,
@@ -119,20 +120,24 @@ void copy_bits(WordType* const dst, WordType const* const src,
 }
 
 template<typename ContextType, typename Rho>
-inline auto merge_bit_vectors(uint64_t size, uint64_t levels, uint64_t shards,
-  const std::vector<ContextType>& src_ctxs, const Rho& rho) {
+inline auto merge_bit_vectors(uint64_t size,
+                              uint64_t levels,
+                              uint64_t shards,
+                              const std::vector<ContextType>& src_ctxs,
+                              const Rho& rho)
+{
   assert(shards == src_ctxs.size());
 
   // Allocate data structures centrally
 
   struct MergeLevelCtx {
     std::vector<uint64_t> read_offsets;
-    uint64_t offset_in_first_word;
+    uint64_t initial_offset;
     uint64_t first_read_block;
   };
 
   struct MergeCtx {
-    uint64_t offset;
+    uint64_t end_offset;
     std::vector<MergeLevelCtx> levels;
   };
 
@@ -147,7 +152,66 @@ inline auto merge_bit_vectors(uint64_t size, uint64_t levels, uint64_t shards,
     },
   }};
 
-  // Calculate bit offset per merge shard (thread)
+  /*
+    Visualization of ctxs for levels = 2 and shards = 2:
+
+    ┌──────────────┬───────────────────────┐
+    │ctxs: MergeCtx│ x shards              │
+    │ ┌────────────┴───────────────────────┤
+    │ │end_offset: uint64_t                │
+    │ ├─────────────────────┬──────────────┤
+    │ │levels: MergeLevelCtx│ x levels     │
+    │ │ ┌───────────────────┴──────────────┤
+    │ │ │initial_offset: uint64_t          │
+    │ │ ├──────────────────────────────────┤
+    │ │ │first_read_block: uint64_t        │
+    │ │ ├──────────────────────┬───────────┤
+    │ │ │read_offsets: uint64_t│ x shards  │
+    │ │ │ ┌────────────────────┴───────────┤
+    │ │ │ │                                │
+    │ │ │ ╞════════════════════════════════╡
+    │ │ │ │                                │
+    │ │ ╞═╧════════════════════════════════╡
+    │ │ │initial_offset: uint64_t          │
+    │ │ ├──────────────────────────────────┤
+    │ │ │first_read_block: uint64_t        │
+    │ │ ├──────────────────────┬───────────┤
+    │ │ │read_offsets: uint64_t│ x shards  │
+    │ │ │ ┌────────────────────┴───────────┤
+    │ │ │ │                                │
+    │ │ │ ╞════════════════════════════════╡
+    │ │ │ │                                │
+    │ ╞═╧═╧════════════════════════════════╡
+    │ │end_offset: uint64_t                │
+    │ ├─────────────────────┬──────────────┤
+    │ │levels: MergeLevelCtx│ x levels     │
+    │ │ ┌───────────────────┴──────────────┤
+    │ │ │initial_offset: uint64_t          │
+    │ │ ├──────────────────────────────────┤
+    │ │ │first_read_block: uint64_t        │
+    │ │ ├──────────────────────┬───────────┤
+    │ │ │read_offsets: uint64_t│ x shards  │
+    │ │ │ ┌────────────────────┴───────────┤
+    │ │ │ │                                │
+    │ │ │ ╞════════════════════════════════╡
+    │ │ │ │                                │
+    │ │ ╞═╧════════════════════════════════╡
+    │ │ │initial_offset: uint64_t          │
+    │ │ ├──────────────────────────────────┤
+    │ │ │first_read_block: uint64_t        │
+    │ │ ├──────────────────────┬───────────┤
+    │ │ │read_offsets: uint64_t│ x shards  │
+    │ │ │ ┌────────────────────┴───────────┤
+    │ │ │ │                                │
+    │ │ │ ╞════════════════════════════════╡
+    │ │ │ │                                │
+    └─┴─┴─┴────────────────────────────────┘
+
+  */
+
+  // Calculate end bit offset per merge shard (thread).
+  // It is an multiple of 64, to ensure no interference
+  // in writing bits to the left or right of it in parallel
   for (size_t rank = 1; rank < shards; rank++) {
     const size_t omp_rank = rank;
     const size_t omp_size = shards;
@@ -155,57 +219,99 @@ inline auto merge_bit_vectors(uint64_t size, uint64_t levels, uint64_t shards,
     const uint64_t offset = (omp_rank * (word_size(size) / omp_size)) +
       std::min<uint64_t>(omp_rank, word_size(size) % omp_size);
 
-    ctxs[rank - 1].offset = offset * 64ull;
+    ctxs[rank - 1].end_offset = offset * 64ull;
   }
-  ctxs[shards - 1].offset = word_size(size) * 64ull;
+  ctxs[shards - 1].end_offset = word_size(size) * 64ull;
 
   //#pragma omp parallel for
   for(size_t level = 0; level < levels; level++) {
-    const size_t br_size = 1ull << level;
+    // number of tree nodes on the current level
+    const size_t blocks = 1ull << level;
 
     size_t write_offset = 0; // bit offset in destination bv
     size_t merge_shard = 0; // index of merge thread
 
-    for(size_t i = 0; i < br_size * shards; i++) {
-      const auto bit_i = i / shards;
-      const auto read_shard = i % shards;
-
-      auto read_offset =
-        [&level, &ctxs](auto merge_shard, auto read_shard) -> uint64_t& {
-          return ctxs[merge_shard].levels[level].read_offsets[read_shard];
+    // iterate over all blocks of all shards on the current level
+    //
+    // this gradually assigns the bits of the
+    // blocks to each of the aviable merge shards of the current level:
+    //
+    // |   read_shard  |   read_shard  |
+    // +---------------+---------------+
+    // |     block     |     block     |
+    // | block | block | block | block | <- current level
+    // +----------+----------+---------+
+    // | m. shard | m. shard |m. shard |
+    //
+    for(size_t i = 0; i < blocks * shards; i++) {
+      // returns merge level context of merge_shard+1
+      auto nxt_lctx = [&level, &ctxs](auto merge_shard) -> MergeLevelCtx& {
+          return ctxs[merge_shard + 1].levels[level];
       };
 
-      auto block_size = src_ctxs[read_shard].hist(level, rho(level, bit_i));
+      // which block (node on current level of tree)
+      const auto block = i / shards;
 
+      // which shard to read from
+      const auto read_shard = i % shards;
+
+      // map logical block index to
+      // actual block index (rho depends on WT vs WM)
+      const auto permuted_block = rho(level, block);
+
+      // block size == number of entries in the block on this level
+      auto block_size = src_ctxs[read_shard].hist(level, permuted_block);
+
+      // advance global write offset by the number of bits assigned for
+      // this block
       write_offset += block_size;
-      read_offset(merge_shard + 1, read_shard) += block_size;
+
+      // advance local read offset of next merge shard
+      // for the curent read shard
+      //
+      // this way, all merge shard start reading
+      // at different offsets from the read shards
+      nxt_lctx(merge_shard).read_offsets[read_shard] += block_size;
 
       // If we passed the current right border, split up the block
-      if (write_offset > ctxs[merge_shard].offset) {
+      if (write_offset > ctxs[merge_shard].end_offset) {
         // Take back the last step
         write_offset -= block_size;
-        read_offset(merge_shard + 1, read_shard) -= block_size;
+        nxt_lctx(merge_shard).read_offsets[read_shard] -= block_size;
 
-        uint64_t offset_in_first_word = 0;
+        uint64_t initial_offset = 0;
         do {
-    // Split up the block like this:
-    //     [  left_block_size  |    right_block_size    ]
-    //     ^             ^                ^
-    // (write_offset)  (ctxs[merge_shard].offset)    (write_offset + block_size)
+          // Split up the block like this:
+          //     [  left_block_size  |    right_block_size    ]
+          //     ^                   ^                        ^
+          // (write_offset)          |                        |
+          //           (ctxs[merge_shard].end_offset)         |
+          //                                     (write_offset + block_size)
+          //
+          // this loop iterates multiple times, to ensure right_block_size
+          // did not also overlap the next end_offset
 
-          auto const left_block_size = ctxs[merge_shard].offset - write_offset;
+          auto const left_block_size
+            = ctxs[merge_shard].end_offset - write_offset;
 
+          // advance global and local read offsets to end exactly at
+          // end_offset
           write_offset += left_block_size;
-          read_offset(merge_shard + 1, read_shard) += left_block_size;
+          nxt_lctx(merge_shard).read_offsets[read_shard] += left_block_size;
 
-          offset_in_first_word += left_block_size;
-          ctxs[merge_shard + 1].levels[level].offset_in_first_word =
-            offset_in_first_word;
-          ctxs[merge_shard + 1].levels[level].first_read_block = i;
+          initial_offset += left_block_size;
 
+          // from which offset in which block to start reading
+          nxt_lctx(merge_shard).initial_offset = initial_offset;
+          nxt_lctx(merge_shard).first_read_block = i;
+
+          // if there is a merge_shard to the right
+          // of the next merge shard, intialize
+          // its local read offsets with those of the next shard
           if (merge_shard + 2 < shards) {
             for(size_t s = 0; s < shards; s++) {
-              read_offset(merge_shard + 2, s) = read_offset(merge_shard + 1, s);
+              nxt_lctx(merge_shard + 1).read_offsets[s]
+                = nxt_lctx(merge_shard).read_offsets[s];
             }
           }
 
@@ -220,20 +326,20 @@ inline auto merge_bit_vectors(uint64_t size, uint64_t levels, uint64_t shards,
           // Iterate on remaining block, because one block might
           // span multiple threads
           block_size -= left_block_size;
-        } while ((write_offset + block_size) > ctxs[merge_shard].offset);
+        } while ((write_offset + block_size) > ctxs[merge_shard].end_offset);
 
         // Process remainder of block
         write_offset += block_size;
-        read_offset(merge_shard + 1, read_shard) += block_size;
+        nxt_lctx(merge_shard).read_offsets[read_shard] += block_size;
 
-        assert(write_offset <= ctxs[merge_shard].offset);
+        assert(write_offset <= ctxs[merge_shard].end_offset);
       }
     }
     triple_loop_exit:; // we are done
   }
 
   auto r = bit_vectors(levels, size);
-  auto& _bv = r.raw_data();
+  auto& _bv = r;
 
   #pragma omp parallel
   //for(size_t omp_rank = 0; omp_rank < shards; omp_rank++)
@@ -244,39 +350,48 @@ inline auto merge_bit_vectors(uint64_t size, uint64_t levels, uint64_t shards,
 
     auto& ctx = ctxs[merge_shard];
 
-    const auto target_right = std::min(ctx.offset, size);
+    const auto target_right = std::min(ctx.end_offset, size);
     const auto target_left = std::min((merge_shard > 0 ?
-      ctxs[merge_shard - 1].offset : 0), target_right);
+      ctxs[merge_shard - 1].end_offset : 0), target_right);
 
     for (size_t level = 0; level < levels; level++) {
-      auto seq_i = ctx.levels[level].first_read_block;
-
+      auto i = ctx.levels[level].first_read_block;
       uint64_t write_offset = target_left;
-      size_t init_offset = ctx.levels[level].offset_in_first_word;
 
-      while (write_offset < target_right) {
-        const auto i = seq_i / shards;
-        const auto read_shard = seq_i % shards;
-        seq_i++;
+      auto copy_next_block = [&](size_t const initial_offset) {
+        const auto block = i / shards;
+        const auto read_shard = i % shards;
+        i++;
 
+        const auto& local_bv = src_ctxs[read_shard].bv()[level];
         const auto& h = src_ctxs[read_shard];
-        const auto& local_bv = src_ctxs[read_shard].bv().raw_data()[level];
+        uint64_t block_size = h.hist(level, rho(level, block)) - initial_offset;
+        uint64_t distance_to_end = target_right - write_offset;
 
-        auto const block_size = std::min<uint64_t>(
-          target_right - write_offset,
-          h.hist(level, rho(level, i)) - init_offset
-        );
-        init_offset = 0; // TODO: remove this by doing a initial pass
+        uint64_t copy_size;
+        if (PWM_LIKELY(block_size <= distance_to_end)) {
+            copy_size = block_size;
+        } else {
+            copy_size = distance_to_end;
+        }
 
         auto& local_cursor = ctx.levels[level].read_offsets[read_shard];
-
         copy_bits<uint64_t>(
-          _bv[level],
-          local_bv,
+          _bv[level].data(),
+          local_bv.data(),
           write_offset,
           local_cursor,
-          block_size
+          copy_size
         );
+      };
+
+      if (write_offset < target_right) {
+        // the first block might start somewhere in the middle
+        copy_next_block(ctx.levels[level].initial_offset);
+      }
+      while (write_offset < target_right) {
+        // other blocks start at 0
+        copy_next_block(0);
       }
     }
   }
