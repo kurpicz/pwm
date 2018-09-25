a5fa4922caf14d61f0dc2b1e9f701794eec32b6c
Marvin Löbel <loebel.marvin@gmail.com>
2018-09-25 18:22:57 +0200

    fix huffman construction test

--- test/construction_tests.cpp
+++ test/construction_tests.cpp
@@ -20,7 +20,7 @@
 #include "util/decode.hpp"
 #include "util/file_util.hpp"
 
-TEST(wavelet_construction, smoketest) {
+TEST(wavelet, no_alphabet_reduction) {
   auto& algo_list = algorithm_list::get_algorithm_list();
   for (const auto& a : algo_list) {
     if (a->word_width() == 1 && !a->is_huffman_shaped()) {
@@ -41,14 +41,43 @@ TEST(wavelet_construction, smoketest) {
   }
 }
 
-TEST(huffman_shaped_wavelet_construction, smoketest) {
+/*
+TODO: Broken due to levels == 0 corner case
+
+TEST(construction, wavelet_alphabet_reduction) {
+  auto& algo_list = algorithm_list::get_algorithm_list();
+  for (const auto& a : algo_list) {
+    if (a->word_width() == 1 && !a->is_huffman_shaped()) {
+      a->print_info();
+      test::roundtrip_batch([&](const std::string& s){
+        auto vec = std::vector<uint8_t>(s.begin(), s.end());
+        auto max_char = reduce_alphabet(vec);
+        uint64_t levels = levels_for_max_char(max_char);
+        wavelet_structure bvz = a->compute_bitvector(&vec, vec.size() , levels);
+        if (a->is_tree()) {
+          auto decoded_s = decode_wt(bvz.bvs(), vec.size());
+          auto decoded_vec = std::vector<uint8_t>(decoded_s.begin(), decoded_s.end());
+          ASSERT_EQ(vec, decoded_vec) << "Failure (Algorithm): " << a->name();
+        } else {
+          auto decoded_s = decode_wm(bvz.bvs(), bvz.zeros(), vec.size());
+          auto decoded_vec = std::vector<uint8_t>(decoded_s.begin(), decoded_s.end());
+          ASSERT_EQ(vec, decoded_vec) << "Failure (Algorithm): " << a->name();
+        }
+      });
+    }
+  }
+}
+*/
+
+TEST(huffman_shaped_wavelet, alphabet_reduction) {
   auto& algo_list = algorithm_list::get_algorithm_list();
   for (const auto& a : algo_list) {
     if (a->word_width() == 1 && a->is_huffman_shaped()) {
       a->print_info();
       test::roundtrip_batch([&](const std::string& s){
         auto vec = std::vector<uint8_t>(s.begin(), s.end());
-        uint64_t levels = no_reduction_alphabet(vec);
+        auto max_char = reduce_alphabet(vec);
+        uint64_t levels = levels_for_max_char(max_char);
         auto bvz = a->compute_bitvector(&vec, vec.size() , levels);
         histogram<uint8_t> hist { vec.data(), vec.size() };
         level_sizes_builder<uint8_t> builder { std::move(hist) };
@@ -56,12 +85,14 @@ TEST(huffman_shaped_wavelet_construction, smoketest) {
           const auto codes =
             canonical_huff_codes<uint8_t, true>(builder);
           auto decoded_s = decode_wt_huff(bvz.bvs(), codes);
-          ASSERT_EQ(s, decoded_s) << "Failure (Algorithm): " << a->name();
+          auto decoded_vec = std::vector<uint8_t>(decoded_s.begin(), decoded_s.end());
+          ASSERT_EQ(vec, decoded_vec) << "Failure (Algorithm): " << a->name();
         } else {
           const auto codes =
             canonical_huff_codes<uint8_t, false>(builder);
           auto decoded_s = decode_wm_huff(bvz.bvs(), codes);
-          ASSERT_EQ(s, decoded_s) << "Failure (Algorithm): " << a->name();
+          auto decoded_vec = std::vector<uint8_t>(decoded_s.begin(), decoded_s.end());
+          ASSERT_EQ(vec, decoded_vec) << "Failure (Algorithm): " << a->name();
         }
       });
     }
