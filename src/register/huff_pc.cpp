b294ead2db3ce56482a036fea8cf3bc06e32d3db
Marvin Löbel <loebel.marvin@gmail.com>
2018-10-01 20:00:36 +0200

    Consistent renaming

--- src/register/huff_pc.cpp
+++ src/register/huff_pc.cpp
@@ -12,26 +12,26 @@
 using wm_huff_pc_8 = wx_huff_pc<uint8_t, false>;
 using wm_huff_pc_16 = wx_huff_pc<uint16_t, false>;
 using wm_huff_pc_32 = wx_huff_pc<uint32_t, false>;
-CONSTRUCTION_REGISTER("huff_wm_pc",
+CONSTRUCTION_REGISTER("wm_huff_pc",
   "Sequential Huffman-shaped wavelet matrix construction with 8-bit"
   " alphabet (using counting).", wm_huff_pc_8)
-CONSTRUCTION_REGISTER("huff_wm_pc",
+CONSTRUCTION_REGISTER("wm_huff_pc",
   "Sequential Huffman-shaped wavelet matrix construction with 16-bit"
   " alphabet (using counting).", wm_huff_pc_16)
-CONSTRUCTION_REGISTER("huff_wm_pc",
+CONSTRUCTION_REGISTER("wm_huff_pc",
   "Sequential Huffman-shaped wavelet matrix construction with 32-bit"
   " alphabet (using counting).", wm_huff_pc_32)
 
 using wt_huff_pc_8 = wx_huff_pc<uint8_t, true>;
 using wt_huff_pc_16 = wx_huff_pc<uint16_t, true>;
 using wt_huff_pc_32 = wx_huff_pc<uint32_t, true>;
-CONSTRUCTION_REGISTER("huff_wt_pc",
+CONSTRUCTION_REGISTER("wt_huff_pc",
   "Sequential Huffman-shaped wavelet tree construction with 8-bit"
   " alphabet (using counting).", wt_huff_pc_8)
-CONSTRUCTION_REGISTER("huff_wt_pc",
+CONSTRUCTION_REGISTER("wt_huff_pc",
   "Sequential Huffman-shaped wavelet tree construction with 16-bit"
   " alphabet (using counting).", wt_huff_pc_16)
-CONSTRUCTION_REGISTER("huff_wt_pc",
+CONSTRUCTION_REGISTER("wt_huff_pc",
   "Sequential Huffman-shaped wavelet tree construction with 32-bit"
   " alphabet (using counting).", wt_huff_pc_32)
 
--- src/register/huff_ps.cpp
+++ src/register/huff_ps.cpp
@@ -12,26 +12,26 @@
 using wm_huff_ps_8 = wx_huff_ps<uint8_t, false>;
 using wm_huff_ps_16 = wx_huff_ps<uint16_t, false>;
 using wm_huff_ps_32 = wx_huff_ps<uint32_t, false>;
-CONSTRUCTION_REGISTER("huff_wm_ps",
+CONSTRUCTION_REGISTER("wm_huff_ps",
   "Sequential Huffman-shaped wavelet matrix construction with 8-bit"
   " alphabet (using sorting).", wm_huff_ps_8)
-CONSTRUCTION_REGISTER("huff_wm_ps",
+CONSTRUCTION_REGISTER("wm_huff_ps",
   "Sequential Huffman-shaped wavelet matrix construction with 16-bit"
   " alphabet (using sorting).", wm_huff_ps_16)
-CONSTRUCTION_REGISTER("huff_wm_ps",
+CONSTRUCTION_REGISTER("wm_huff_ps",
   "Sequential Huffman-shaped wavelet matrix construction with 32-bit"
   " alphabet (using sorting).", wm_huff_ps_32)
 
 using wt_huff_ps_8 = wx_huff_ps<uint8_t, true>;
 using wt_huff_ps_16 = wx_huff_ps<uint16_t, true>;
 using wt_huff_ps_32 = wx_huff_ps<uint32_t, true>;
-CONSTRUCTION_REGISTER("huff_wt_ps",
+CONSTRUCTION_REGISTER("wt_huff_ps",
   "Sequential Huffman-shaped wavelet tree construction with 8-bit"
   " alphabet (using sorting).", wt_huff_ps_8)
-CONSTRUCTION_REGISTER("huff_wt_ps",
+CONSTRUCTION_REGISTER("wt_huff_ps",
   "Sequential Huffman-shaped wavelet tree construction with 16-bit"
   " alphabet (using sorting).", wt_huff_ps_16)
-CONSTRUCTION_REGISTER("huff_wt_ps",
+CONSTRUCTION_REGISTER("wt_huff_ps",
   "Sequential Huffman-shaped wavelet tree construction with 32-bit"
   " alphabet (using sorting).", wt_huff_ps_32)
 
