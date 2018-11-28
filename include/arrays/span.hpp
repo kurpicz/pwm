9434d2bdbe2acffc54229fd29606de5c585ca237
Florian Kurpicz <florian.kurpicz@tu-dortmund.de>
2018-11-28 13:32:16 +0100

    Use nullptr in include/arrays/span.hpp
    Co-Authored-By: Kimundi <loebel.marvin@gmail.com>

--- include/arrays/span.hpp
+++ include/arrays/span.hpp
@@ -19,7 +19,7 @@ class span {
 
 public:
   inline span(T* ptr, size_t size) : ptr_(ptr), size_(size) { }
-  inline span(): span((T*) "", 0) {}
+  inline span(): span(nullptr, 0) {}
 
   inline size_t size() const {
     return size_;
