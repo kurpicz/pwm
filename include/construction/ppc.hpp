ac9be74e7e6ba62f49150b84ece3f699e49102b9
Marvin Löbel <loebel.marvin@gmail.com>
2019-03-08 17:29:52 +0100

    refactor ppc to use a two dim array and obey default thread num limit

--- include/construction/ppc.hpp
+++ include/construction/ppc.hpp
@@ -26,81 +26,62 @@ void ppc(AlphabetType const* text,
   // compute initial histogram
   {
     const uint64_t alphabet_size = (1ull << levels);
-    std::vector<uint64_t> initial_hist_v(alphabet_size * levels, 0);
-    span<uint64_t> initial_hist{initial_hist_v.data(), initial_hist_v.size()};
-    auto&& hist = ctx.hist_at_level(levels);
 
-    #pragma omp parallel num_threads(levels)
+    // Allocate a histogram buffer per thread
+    helper_array sharded_hists(omp_get_max_threads(), alphabet_size);
+
+    // Write the first level, and fill the histograms in parallel
+    #pragma omp parallel
     {
-      const uint64_t omp_rank = uint64_t(omp_get_thread_num());
-      DCHECK(levels == uint64_t(omp_get_num_threads()));
-
-      auto initial_hist_ptr = initial_hist.slice(
-          alphabet_size * omp_rank, alphabet_size * (omp_rank + 1));
+      const auto shard = omp_get_thread_num();
+      auto&& hist = sharded_hists[shard];
 
       omp_write_bits_wordwise(0, size, bv[0], [&](uint64_t const i) {
         auto const c = text[i];
-        initial_hist_ptr[c]++;
-        uint64_t bit = ((c >> (levels - 1)) & 1ULL);
+        hist[c]++;
+        uint64_t const bit = ((c >> (levels - 1)) & 1ULL);
         return bit;
       });
+    }
 
-      #pragma omp barrier
-
-      // Compute the histogram with respect to the local slices of the text
-      #pragma omp for
-      for (uint64_t i = 0; i < alphabet_size; ++i) {
-        for (uint64_t rank = 0; rank < levels; ++rank) {
-          hist[i] += initial_hist[(alphabet_size * rank) + i];
-        }
+    // Accumulate the histograms
+    auto&& hist = ctx.hist_at_level(levels);
+    #pragma omp parallel for
+    for (uint64_t j = 0; j < alphabet_size; ++j) {
+      for (uint64_t shard = 0; shard < sharded_hists.levels(); ++shard) {
+        hist[j] += sharded_hists[shard][j];
       }
     }
   }
 
   auto&& leaf_hist = ctx.hist_at_level(levels);
 
-  #pragma omp parallel num_threads(levels)
-  {
-    [[maybe_unused]]const uint64_t omp_rank_ = omp_get_thread_num();
-    DCHECK(omp_rank_ < levels);
-
-    // Compute the histogram for each level of the wavelet structure
-    #pragma omp for
-    for (uint64_t level = 1; level < levels; ++level) {
-      // TODO: remove again
-      [[maybe_unused]]const uint64_t omp_rank = omp_get_thread_num();
-      DCHECK(omp_rank < levels);
-      DCHECK(omp_rank == omp_rank_);
-
-      auto&& hist = ctx.hist_at_level(level);
-      const uint64_t blocks = (1 << level);
-      const uint64_t required_characters = (1 << (levels - level));
-      for (uint64_t i = 0; i < blocks; ++i) {
-        for (uint64_t j = 0; j < required_characters; ++j) {
-          hist[i] += leaf_hist[(i * required_characters) + j];
-        }
+  // Compute the histogram for each level of the wavelet structure
+  #pragma omp parallel for schedule(nonmonotonic : dynamic, 1)
+  for (uint64_t level = 1; level < levels; ++level) {
+    auto&& hist = ctx.hist_at_level(level);
+    const uint64_t blocks = (1 << level);
+    const uint64_t required_characters = (1 << (levels - level));
+    for (uint64_t i = 0; i < blocks; ++i) {
+      for (uint64_t j = 0; j < required_characters; ++j) {
+        hist[i] += leaf_hist[(i * required_characters) + j];
       }
     }
+  }
 
-    // Now we compute the wavelet structure bottom-up, i.e., the last level
-    // first
-    #pragma omp for
-    for (uint64_t level = 1; level < levels; ++level) {
-      // TODO: remove again
-      const uint64_t omp_rank = omp_get_thread_num();
-      DCHECK(omp_rank < levels);
-      DCHECK(omp_rank == omp_rank_);
+  // Now we compute the wavelet structure bottom-up, i.e., the last level
+  // first
+  #pragma omp parallel for schedule(nonmonotonic : dynamic, 1)
+  for (uint64_t level = 1; level < levels; ++level) {
+    const uint64_t blocks = (1 << level);
 
-      const uint64_t blocks = (1 << level);
+    auto borders = ctx.borders_at_shard(level).slice(0, blocks);
 
-      auto borders = ctx.borders_at_shard(omp_rank).slice(0, blocks);
+    compute_borders_optional_zeros_rho(level, blocks, ctx, borders);
 
-      compute_borders_optional_zeros_rho(level, blocks, ctx, borders);
-
-      // Now we insert the bits with respect to their bit prefixes
-      for (uint64_t i = 0; i < size; ++i) {
-        write_symbol_bit(bv, level, levels, borders, text[i]);
-      }
+    // Now we insert the bits with respect to their bit prefixes
+    for (uint64_t i = 0; i < size; ++i) {
+      write_symbol_bit(bv, level, levels, borders, text[i]);
     }
   }
 
